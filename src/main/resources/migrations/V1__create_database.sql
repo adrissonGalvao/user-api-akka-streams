936fa7f7f33c705bc77d3632907b13b89d665db5
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-04-08 11:15:58 -0300

    feature: create and find (must be refactored)

--- src/main/resources/migrations/V1__create_database.sql
+++ src/main/resources/migrations/V1__create_database.sql
@@ -1,4 +1,7 @@
-CREATE TABLE  tb_user (
+create schema user_db;
+
+
+CREATE TABLE  user_db.tb_user (
  id serial PRIMARY KEY,
  name VARCHAR (50) NOT NULL,
  username VARCHAR (50) UNIQUE NOT NULL,
--- src/main/scala/repository/UserRepository.scala
+++ src/main/scala/repository/UserRepository.scala
@@ -21,7 +21,7 @@ class UserRepositoryImpl extends UserRepository {
   def findById(id: Long): Option[User] = {
 
     val q = quote {
-      query[User].filter(_.id == lift(id))
+      querySchema[User]("user_db.tb_user").filter(_.id == lift(id))
     }
 
     ctx.run(q).headOption
@@ -30,7 +30,7 @@ class UserRepositoryImpl extends UserRepository {
   def insert(createUser: CreateUser): User = {
 
     val q = quote {
-      query[User]
+      querySchema[User]("user_db.tb_user")
         .insert(lift(
           User(-1, createUser.name, createUser.username, createUser.birthDate)))
         .returning(_.id)
